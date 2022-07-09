d20e6ab554453e23a7f28247bce2f26e4ce1fad4
Ariana Carvalho <arianaacarvalho27@gmail.com>
2022-07-09 18:27:41 -0300

    Adiciona NonBloking, duração da função e separador

--- BlokingVsNonBloking.js
+++ BlokingVsNonBloking.js
@@ -1,7 +1,11 @@
 //Importando funcionalidades de leitura e cópia 
 const {
+    //Bloking
     readFileSync, 
     writeFileSync,
+    //NonBloking
+    readFile,
+    writeFile,
 } = require('fs'); //File System
 
 //Importando funcionalidade para achar o caminho do arquivo
@@ -9,15 +13,51 @@ const{
     join, //Faz a distinsão de sistemas operacionais
 } = require('path');
 
-//função copia e escrita
+const logduration = (label, startTime) => {
+    console.log(`${label} levou ${Date.now() - startTime}ms`);
+}
+
+//função copia e escrita - Bloking
 const copyFileBloking = (source, dest) => {
+    const startTime = Date.now();
     console.log('Lendo Bloking conteúdo');
     const content = readFileSync(source);
     console.log('Escrevendo Bloking conteúdo');
     writeFileSync(dest, content);
+logduration('copyFileBloking', startTime);
 };
 
 const sourcePath = join(__dirname, 'files', 'exemplo.txt');
 const destPath = join(__dirname, 'files', 'exemple.copy.bloking.txt');
 
 copyFileBloking(sourcePath, destPath);
+console.log("Cópia Bloking com sucesso!");
+
+console.log("*".repeat(50));
+
+//função copia e escrita - NonBloking
+const copyFileNonBloking = (source, dest) => {
+    const startTime = Date.now();
+    console.log("Começou a cópia Non-Bloking");
+            
+        readFile(source, (_err,data) => {
+        console.log("Terminou de ler Non-Bloking");
+            
+            writeFile(dest, data, (_err) => {
+            console.log("Terminou de escrever Non-Bloking");
+            logduration('copyFileNonBloking', startTime);
+            })
+        })
+    }
+    
+    const destNonBlokin = join(__dirname, 'files', 'exemple.copy.NonBloking.txt');
+    
+    copyFileNonBloking(sourcePath, destNonBlokin);
+
+    console.log("Terminou mesmo?");
+
+    console.log(
+        "Continuando...",
+        1+1,
+        Math.PI * Math.E,
+    );
